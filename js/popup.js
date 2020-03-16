1879575bf43b3b8dac1f633db2496e1b15404834
playfulno <60173955+playfulno@users.noreply.github.com>
2020-03-16 19:15:04 +0300

    Личный проект
    - модальное окно
    - карта
    - js

---
+++ js/popup.js
@@ -0,0 +1,47 @@
+var link = document.querySelector("write-button");
+var popup = document.querySelector(".modal-post");
+var close = popup.querySelector(".modal-close");
+
+var form = popup.querySelector("form");
+var name = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function(evt){
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    text.focus();
+
+});
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+});
+
+form.addEventListener("submit", function (evt) {
+    if (!email.value || !text.value) {
+      evt.preventDefault();
+      console.log("Нужно ввести имя и э-мэйл");
+    } else {
+        localStorage.setItem("email", email.value);
+      }
+});
+
+
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
+    }
+  });
